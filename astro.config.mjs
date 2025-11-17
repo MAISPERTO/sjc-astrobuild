cc9e3db319abecab4a56811922f3587d23f0c9fd
MAISPERTO <mpgomes2024@outlook.com.br>
2025-11-17 06:26:35 -0300

    Refatoracao: Centraliza logica Directus em src/lib/directus.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'; // Linha 1: Importa o adaptador Node
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // Linha 2: Define o modo de renderização como SSR
+  adapter: node({ // Linha 3: Adiciona o adaptador
+    mode: 'standalone'
+  })
+});
