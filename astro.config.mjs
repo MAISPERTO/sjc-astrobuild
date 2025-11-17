80f06df3c30a329daddcbd7ce006d5fe34947208
MAISPERTO <mpgomes2024@outlook.com.br>
2025-11-17 10:02:10 -0300

    feat: Adicionado .env e ajustado codigo para usar PUBLIC_CITY_SLUG=sjc e variaveis de ambiente. Pronta para Coolify.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'; // Linha 1: Importa o adaptador Node
+import node from '@astrojs/node'; // 1. Importe o adapter
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Linha 2: Define o modo de renderização como SSR
-  adapter: node({ // Linha 3: Adiciona o adaptador
+  output: 'server', // 2. Defina a saída como 'server'
+  adapter: node({ // 3. Configure o adapter node
     mode: 'standalone'
   })
 });
